0fe16a13b87375ae26eca9dea34388506656e772
Piotr Ledwoch <245859@edu.p.lodz.pl>
2025-03-21 15:22:33 +0100

    Add ConsolePlayer agent and an option to choose actions in console

--- poker-enviroment/agents/random_agent.py
+++ poker-enviroment/agents/random_agent.py
@@ -23,16 +23,16 @@ class RandomAgent(IAgent):
             return (Action.FOLD, None)
         elif rand_value < 0.9:
             # This is a simple strategy to avoid going all-in when the call amount is higher than 1/3 of player's chips.
-            if observation.get("call_amount",200) > observation.get("chips", 1000)//3 and random.random() < 0.5:
+            if observation.get("call_amount") > observation.get("chips")//3 and random.random() < 0.5:
                 return (Action.FOLD, None)
 
             return (Action.CALL, None)
         else:
-            chips: int = observation.get("chips", 1000)
+            chips: int = observation.get("chips")
 
             # minimum raise is big blind amount, or call amount if it's larger
             # it may be needed to add big blind amount to observation
-            min_raise = observation.get("call_amount", 20) if observation.get("call_amount", 20) > 0 else 20
+            min_raise = observation.get("call_amount") if observation.get("call_amount") > 0 else 20
             
             random_raise: float = random.random()
             raise_amount = min_raise

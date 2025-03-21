fee8822d39af9fe5637f3887c9b2c75ad5a9b9a6
Piotr Ledwoch <245859@edu.p.lodz.pl>
2025-03-21 14:08:39 +0100

    Enhance RandomAgent's action logic and update PokerEnv observation structure

--- poker-enviroment/agents/random_agent.py
+++ poker-enviroment/agents/random_agent.py
@@ -16,24 +16,30 @@ class RandomAgent(IAgent):
                 - Action: The chosen action (FOLD, CALL, or RAISE).
                 - Optional[int]: The raise amount if the action is RAISE; otherwise, None.
         """
-        # Probability distribution: 5% FOLD, 85% CALL, 9% RAISE, 1% ALL-IN.
+        # Probability distribution: 5% FOLD, 85% CALL, 10% RAISE(1% ALL-IN, 79% 1-5 times call amount, 20% call amount).
         
         rand_value: float = random.random()
         if rand_value < 0.05:
             return (Action.FOLD, None)
         elif rand_value < 0.9:
-            # if pot is larger than chips, fold with 50% chance
-            # This is a simple strategy to avoid going all-in when the pot is larger than the player's chips.
-            if observation.get("pot", 30) > observation.get("chips", 1000) and random.random() < 0.5:
+            # This is a simple strategy to avoid going all-in when the call amount is higher than 1/3 of player's chips.
+            if observation.get("call_amount",200) > observation.get("chips", 1000)//3 and random.random() < 0.5:
                 return (Action.FOLD, None)
 
             return (Action.CALL, None)
         else:
             chips: int = observation.get("chips", 1000)
+
+            # minimum raise is big blind amount, or call amount if it's larger
+            # it may be needed to add big blind amount to observation
+            min_raise = observation.get("call_amount", 20) if observation.get("call_amount", 20) > 0 else 20
             
+            random_raise: float = random.random()
+            raise_amount = min_raise
+
             if random.random() < 0.1:
                 return (Action.RAISE, chips)    
+            elif random_raise < 0.8:
+                raise_amount = min_raise * random.randint(1, 5)
             
-            max_raise = min(50, chips) if chips > 0 else 1
-            raise_amount = random.randint(1, max_raise)
             return (Action.RAISE, raise_amount)

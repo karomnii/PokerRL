59757aca3d91a29c374854c4ef128da6122593e9
Piotr Ledwoch <245859@edu.p.lodz.pl>
2025-05-28 19:37:25 +0200

    Fixed round advancement with AllIn plays

--- backend/TexasHoldemPoker.API/TexasHoldemPoker.API/Services/PokerGameService.cs
+++ backend/TexasHoldemPoker.API/TexasHoldemPoker.API/Services/PokerGameService.cs
@@ -377,7 +377,7 @@ namespace TexasHoldemPoker.API.Services
                         effectiveAmount = 0;
                         actionType = "Check";
                     }
-                    else if (callAmount == gamePlayer.CurrentChips)
+                    else if (callAmount >= gamePlayer.CurrentChips)
                     {
                         effectiveAmount = gamePlayer.CurrentChips;
                         actionType = "AllIn";
@@ -392,7 +392,7 @@ namespace TexasHoldemPoker.API.Services
                     break;
                 case "Bet":
                     isValidMove = (callAmount == 0 &&
-                                   amount > (await gameRepository.GetGameTableAsync(gameId)).BigBlind &&
+                                   amount >= (await gameRepository.GetGameTableAsync(gameId)).BigBlind &&
                                    amount <= gamePlayer.CurrentChips);
                     if (isValidMove) effectiveAmount = amount;
                     break;
@@ -639,6 +639,16 @@ namespace TexasHoldemPoker.API.Services
                     await DetermineWinnerAsync(gameId);
                 }
             }
+
+            var activePlayersWithChips = activePlayers.Where(p => p.CurrentChips > 0).ToList();
+            gameRound = await gameRoundRepository.GetCurrentRoundAsync(gameId);
+
+            if (activePlayersWithChips.Count < 2 && (gameRound.CurrentState == "Flop" ||
+                                                     gameRound.CurrentState == "Turn" ||
+                                                     gameRound.CurrentState == "River"))
+            {
+                await AdvanceGameStateAsync(gameId);
+            }
         }
 
         private async Task DealCommunityCardsAsync(int gameId, int count)
@@ -724,6 +734,7 @@ namespace TexasHoldemPoker.API.Services
             {
                 if (winnerIds.Count == 1)
                 {
+                    // TODO: Handle case with AllIn player having less chips(player can win max 2 times the amount of chips if he did not match the bet)
                     await gameRoundWinnerRepository.AddWinnerAsync(gameRound.GameRoundId, winnerIds.First(),
                         totalPot);
                 }
